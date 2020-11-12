44e5cb58b176649f73cfdc577ea298881dc3b79e
niulijuan <niulijuan@ksjgs.com>
2020-11-12 10:33:18 +0800

    添加注释

--- src/main/java/com/niulijuan/algorithmdemo/algorithmdemo/AlgorithmDemoApplication.java
+++ src/main/java/com/niulijuan/algorithmdemo/algorithmdemo/AlgorithmDemoApplication.java
@@ -3,6 +3,9 @@ package com.niulijuan.algorithmdemo.algorithmdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author niulijuan
+ */
 @SpringBootApplication
 public class AlgorithmDemoApplication {
 
---
+++ src/main/java/com/niulijuan/algorithmdemo/algorithmdemo/example/Example1.java
@@ -0,0 +1,16 @@
+package com.niulijuan.algorithmdemo.algorithmdemo.example;
+
+/**
+ * 第（前）k大数问题
+ * <p>
+ * 在长度为n(n>=k)的乱序数组中S找出从大到小顺序的第（前）k个数
+ *
+ * @author niulijuan
+ * @date 2020/11/12 10:25 上午
+ */
+public class Example1 {
+
+    //方法一 数组排序，根据下标取值  吧v 吧宝贝版本v 吧v 不过还好尴尬好尴尬并不尴尬好尴尬尴尬吧把 v 宝贝宝贝宝贝宝贝宝贝宝贝宝贝
+    //方法二 设置K长度数组，使用插入排序，只保留最大n个数
+    //方法三 使用堆排序
+}
